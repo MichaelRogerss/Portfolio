0ce739429163e1c07d1bf81ad538306f5b58e52a
Michael <michael_ro48@yahoo.com>
2022-03-27 14:30:18 -0400

    Updates

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 
-(function() {
-    var burger = document.querySelector('.burger');
-    var menu = document.querySelector('#'+burger.dataset.target);
-    burger.addEventListener('click', function() {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
-    });
-})();
 
 $(document).ready(function (){
+
+            (function() {
+                var burger = document.querySelector('.burger');
+                var menu = document.querySelector('#'+burger.dataset.target);
+                burger.addEventListener('click', function() {
+                    burger.classList.toggle('is-active');
+                    menu.classList.toggle('is-active');
+                });
+            })();
             console.log("done")
         });
